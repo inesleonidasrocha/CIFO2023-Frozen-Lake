431cd7160bd41b76245023cff31065fa6607496c
inesleonidasrocha <ines.l.x.e.f.rocha@gmail.com>
2023-05-25 15:56:14 +0100

    scramble_mutation added

--- mutation.py
+++ mutation.py
@@ -1,28 +1,5 @@
 from random import randint, sample
-
-
-def binary_mutation(individual):
-    """Binary mutation for a GA individual. Flips the bits.
-
-    Args:
-        individual (Individual): A GA individual from charles.py
-
-    Raises:
-        Exception: When individual is not binary encoded.py
-
-    Returns:
-        Individual: Mutated Individual
-    """
-    mut_index = randint(0, len(individual) - 1)
-
-    if individual[mut_index] == 0:
-        individual[mut_index] = 1
-    elif individual[mut_index] == 1:
-        individual[mut_index] = 0
-    else:
-        raise Exception(
-            f"Trying to do binary mutation on {individual}. But it's not binary.")
-    return individual
+import random
 
 
 def swap_mutation(individual):
@@ -52,6 +29,28 @@ def inversion_mutation(individual):
     mut_indexes.sort()
     individual[mut_indexes[0]:mut_indexes[1]] = individual[mut_indexes[0]:mut_indexes[1]][::-1]
     return individual
+
+
+def scramble_mutation(individual):
+    """Scrambled mutation for a GA individual. Scrambles a portion of the representation.
+
+    Args:
+        individual (Individual): A GA individual from charles.py
+
+    Returns:
+        Individual: Mutated Individual
+    """
+    #here we chose a random window 
+    mut_indexes = sample(range(0, len(individual)), 2)
+    mut_indexes.sort()
+    # Scramble the selected section       
+    individual[mut_indexes[0]:mut_indexes[1]] = random.sample(individual[mut_indexes[0]:mut_indexes[1]], mut_indexes[1] - mut_indexes[0]) 
+    return  individual
+
+
+'''if __name__ == '__main__':
+    test = [0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]
+    test = scramble_mutation(test)'''
 
 
 
