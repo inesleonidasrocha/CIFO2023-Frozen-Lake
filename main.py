64908832ef58e53bc41c31c1de4222bcd5819faa
lota97 <93428766+lota97@users.noreply.github.com>
2023-05-27 17:23:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,46 +11,24 @@ Individual.get_fitness = get_fitness
 
 fitness_experiments = []
 
-def run_experiment(population_size, 
-                   generations, 
-                   crossover_probability, 
-                   mutation_probability, 
-                   selection, 
-                   mutation, 
-                   crossover, 
-                   elitism, 
-                   replacement, 
-                   slippery,
-                   sol_size=16,
-                   valid_set=[0, 1, 2, 3]
-                   ):
+def run_experiment(population_size, generations, crossover_probability, mutation_probability, selection, mutation, 
+                   crossover, elitism, replacement, slippery, sol_size = 16, valid_set = [0, 1, 2, 3]):
 
     iterations_fitness = []
     for i in range(50):
         start_time = time.time()
         
         # Create a population
-        pop = Population(
-            size=population_size,
-            optim="max",
-            sol_size=sol_size,
-            valid_set=valid_set,
-            replacement=replacement,
-            slippery=slippery
-        )
+        pop = Population(size = population_size, optim = "max", sol_size = sol_size, valid_set = valid_set,
+                         replacement = replacement, slippery = slippery)
 
         # Evolve the population for 30 iterations
-        iterations_fitness.append(pop.evolve(gens=generations, 
-                                            xo_prob=crossover_probability, 
-                                            mut_prob=mutation_probability, 
-                                            select=selection,
-                                            mutate=mutation, 
-                                            crossover=crossover,
-                                            elitism=elitism))
+        iterations_fitness.append(pop.evolve(gens = generations, xo_prob=crossover_probability, mut_prob = mutation_probability,
+                                             select=selection, mutate = mutation, crossover = crossover, elitism = elitism))
         
         end_time = time.time()  # Stop measuring the time
         iteration_time = end_time - start_time
-        print(f"Iteration {i+1} time: {round(iteration_time,2)} seconds")
+        print(f"Iteration {i + 1} time: {round(iteration_time, 2)} seconds")
         
     # Average iterations by generation
     iterations_fitness_average = list(zip(*iterations_fitness))
@@ -59,20 +37,69 @@ def run_experiment(population_size,
     return iterations_fitness_average
 
 # Experiments configuration
-experiments = [
-    [50, 100, 0.8, 0.2, tournament_sel, scramble_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, tournament_sel, swap_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, tournament_sel, inversion_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, roulette_selection, scramble_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, roulette_selection, swap_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, roulette_selection, inversion_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, rank_selection, scramble_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, rank_selection, swap_mutation, arithmetic_xo, True, True, False],
-    [50, 100, 0.8, 0.2, rank_selection, inversion_mutation, arithmetic_xo, True, True, False]
-]
+# Experiments configuration
+experiments = [# [50, 100, 0.9, 0.2, tournament_sel, scramble_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, scramble_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, scramble_mutation, multi_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, swap_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, swap_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, swap_mutation, multi_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, inversion_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, inversion_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, inversion_mutation, multi_point_xo, False, True, False]
 
-# create a list using the columns of the experiments list
-# this is done to make the code more readable
+               # [50, 100, 0.9, 0.2, roulette_selection, scramble_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, scramble_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, scramble_mutation, multi_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, swap_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, swap_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, swap_mutation, multi_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, inversion_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, inversion_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, inversion_mutation, multi_point_xo, False, True, False]
+
+               # [50, 100, 0.9, 0.2, rank_selection, scramble_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, scramble_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, scramble_mutation, multi_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, swap_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, swap_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, swap_mutation, multi_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, inversion_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, inversion_mutation, single_point_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, inversion_mutation, multi_point_xo, False, True, False]
+
+               # [50, 100, 0.9, 0.2, tournament_sel, scramble_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, swap_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, inversion_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, scramble_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, swap_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, inversion_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, scramble_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, swap_mutation, arithmetic_xo, False, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, inversion_mutation, arithmetic_xo, False, True, False]
+
+               # [50, 100, 0.9, 0.2, tournament_sel, scramble_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, swap_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, tournament_sel, inversion_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, scramble_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, swap_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, roulette_selection, inversion_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, scramble_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, swap_mutation, arithmetic_xo, True, True, False],
+               # [50, 100, 0.9, 0.2, rank_selection, inversion_mutation, arithmetic_xo, True, True, False]
+
+               [50, 100, 0.8, 0.2, tournament_sel, scramble_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, tournament_sel, swap_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, tournament_sel, inversion_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, roulette_selection, scramble_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, roulette_selection, swap_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, roulette_selection, inversion_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, rank_selection, scramble_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, rank_selection, swap_mutation, arithmetic_xo, False, True, False],
+               [50, 100, 0.8, 0.2, rank_selection, inversion_mutation, arithmetic_xo, False, True, False]
+               ]
+
+# List using the columns of the experiments list, this is done to make the code more readable
 population_size = [exp[0] for exp in experiments]
 generations = [exp[1] for exp in experiments]
 crossover_probability = [exp[2] for exp in experiments]
@@ -84,27 +111,20 @@ elitism = [exp[7] for exp in experiments]
 replacement = [exp[8] for exp in experiments]
 slippery = [exp[9] for exp in experiments]
 
-# for each variable, find the unique values
 variable_list = [population_size, generations, crossover_probability, mutation_probability, 
                  selection, mutation, crossover, elitism, replacement, slippery]
 
-# find the first variable that has more than one unique value
 selection_list = []
 for variable in variable_list:
     if len(set(variable)) >= 2:
         selection_list.append(variable)
 
-# 
 label_list = []
 for l in range(len(selection_list[0])):
     label = ([v[l] for v in selection_list])
-    # create a string with the elements in the list
     label = " & ".join(label)
-    # insert label in a list
     label_list.append(label)
     
-
-
 # Run the experiments
 for exp in experiments:
     print(f"Running experiment: {experiments.index(exp)+1}")
@@ -118,22 +138,19 @@ for exp in experiments:
                                               elitism = exp[7],
                                               replacement = exp[8],
                                               slippery = exp[9]))
-    print(f"Experiment {experiments.index(exp)+1} finished")
+    print(f"Experiment {experiments.index(exp) + 1} finished")
     print("--------------------------------------------------")
 
 # Plot the fitness by experiment
 for exp in fitness_experiments:
     plt.plot(exp)
     
+plt.title("Fitness by Generation")
 plt.xlabel("Generation") 
 plt.ylabel("Fitness")
-plt.title("Fitness by Generation")
-# set the left and right margins
+# Set the left and right margins
 plt.subplots_adjust(left=0.08, right=0.73)
-# add a legend outside the plot
+# Add a legend outside the plot
 plt.legend(label_list, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-# show the plot
+# Show the plot
 plt.show()
-
-
-    
