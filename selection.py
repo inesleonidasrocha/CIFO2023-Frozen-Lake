31b56cf8d81b784629357e3924c3d13b27f91fe6
lota97 <93428766+lota97@users.noreply.github.com>
2023-05-27 17:32:06 +0100

    Update selection.py

--- selection.py
+++ selection.py
@@ -1,10 +1,9 @@
-import random
-from random import uniform, choice
+from random import random, uniform, choice
 from operator import attrgetter
 
-
 def tournament_sel(population, size=4):
-    """Tournament selection implementation.
+    """
+    Tournament selection implementation.
 
     Args:
         population (Population): The population we want to select from.
@@ -14,55 +13,54 @@ def tournament_sel(population, size=4):
         Individual: The best individual in the tournament.
     """
 
-    # Select individuals based on tournament size
-    # with choice, there is a possibility of repetition in the choices,
+    # Select individuals based on tournament size with choice, there is a possibility of repetition in the choices,
     # so every individual has a chance of getting selected
     tournament = [choice(population.individuals) for _ in range(size)]
 
-    # with sample, there is no repetition of choices
+    # With sample, there is no repetition of choices
     # tournament = sample(population.individuals, size)
     if population.optim == "max":
-        return max(tournament, key=attrgetter("fitness"))
+        return max(tournament, key = attrgetter("fitness"))
     if population.optim == "min":
-        return min(tournament, key=attrgetter("fitness"))
+        return min(tournament, key = attrgetter("fitness"))
 
 
 def roulette_selection(population):
-    # Step 1: Determine the range of fitness values
+    # Determine the range of fitness values
     min_fitness = min(population.individuals, key=attrgetter("fitness")).fitness
     
-    # Step 2: Adjust the fitness values substracting minimum fitness
+    # Adjust the fitness values substracting minimum fitness
     # Worst value can be picked because we dont want to lose diversity. The number is lower than the rest (1)
     adjusted_fitness = [chromo.fitness - min_fitness +1 for chromo in population.individuals]
 
-    # Step 3: Calculate the total fitness
+    # Calculate the total fitness
     total_fitness = sum(adjusted_fitness)       
 
-    # Step 4: Generate a random number
+    # Generate a random number
     random_num = random.uniform(0, total_fitness)
 
-    # Step 5: Perform roulette wheel selection
+    # Perform roulette wheel selection
     cumulative_sum = 0
     for i, fitness in enumerate(adjusted_fitness):
         cumulative_sum += fitness
         if cumulative_sum >= random_num:
             return population.individuals[i]  # Return the selected individual
 
+        
 def rank_selection(population):
-    # Step 1: Sort the population by fitness
+    # Sort the population by fitness
     sorted_population = sorted(population.individuals, key=attrgetter("fitness"), reverse=True)
     
-    # Step 2: Calculate cumulative probabilities for each individual based on their ranks.
+    # Calculate cumulative probabilities for each individual based on their ranks.
     total_ranks = sum([i for i in range(1, len(sorted_population) + 1)])
     cumulative_probabilities = [(len(sorted_population) - i) / total_ranks for i in range(0, len(sorted_population))]
     
-    # Step 3: Generate a random number
+    # Generate a random number
     random_num = random.uniform(0, 1)
     
-    # Step 4: Perform rank selection
+    # Perform rank selection
     cumulative_sum = 0
     for i, chromo in enumerate(sorted_population):
         cumulative_sum += cumulative_probabilities[i]
         if cumulative_sum >= random_num:
             return chromo
-        
