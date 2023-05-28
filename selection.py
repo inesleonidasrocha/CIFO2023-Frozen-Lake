3666706c37f1f39a4a11d7af3fe04e72dc4db46d
inesleonidasrocha <ines.l.x.e.f.rocha@gmail.com>
2023-05-28 17:56:24 +0100

    Selection methods updated for minimization problem

--- selection.py
+++ selection.py
@@ -4,7 +4,7 @@ from operator import attrgetter
 
 def tournament_sel(population, size=4):
     """
-    Tournament selection implementation.
+    Performs tournament selection to select an individual from the population.
 
     Args:
         population (Population): The population we want to select from.
@@ -18,8 +18,7 @@ def tournament_sel(population, size=4):
     # so every individual has a chance of getting selected
     tournament = [choice(population.individuals) for _ in range(size)]
 
-    # With sample, there is no repetition of choices
-    # tournament = sample(population.individuals, size)
+    # Select the best individual from the tournament
     if population.optim == "max":
         return max(tournament, key = attrgetter("fitness"))
     if population.optim == "min":
@@ -27,41 +26,61 @@ def tournament_sel(population, size=4):
 
 
 def roulette_selection(population):
+    """
+    Perform roulette wheel selection to select an individual from the population.
+       
+    Args:     
+        population (Population): The population to select from  
+    Returns:     
+        Individual: The selected individual    
+    """
     # Determine the range of fitness values
     min_fitness = min(population.individuals, key=attrgetter("fitness")).fitness
     
-    # Adjust the fitness values substracting minimum fitness
-    # Worst value can be picked because we dont want to lose diversity. The number is lower than the rest (1)
+    # Adjust fitness values to prevent worst individuals from being excluded. 
+    # Normalize fitness values to preserve diversity
     adjusted_fitness = [chromo.fitness - min_fitness +1 for chromo in population.individuals]
 
     # Calculate the total fitness
     total_fitness = sum(adjusted_fitness)       
 
-    # Generate a random number
+    # Generate a random number between 0 and total fitness 
     random_num = random.uniform(0, total_fitness)
-
-    # Perform roulette wheel selection
-    cumulative_sum = 0
-    for i, fitness in enumerate(adjusted_fitness):
-        cumulative_sum += fitness
-        if cumulative_sum >= random_num:
-            return population.individuals[i]  # Return the selected individual
+      
+    cumulative_sum = 0           
+    for i, fitness in enumerate(adjusted_fitness):      
+        cumulative_sum += fitness              
+        if cumulative_sum >= random_num:        
+            if population.optim == "min":
+                return min(population.individuals, key=attrgetter("fitness"))  
+            else:
+                return population.individuals[i] 
 
         
 def rank_selection(population):
-    # Sort the population by fitness
+    """
+    Performs rank selection to select an individual from the population.
+   
+    Args:     
+        population (Population): The population to select from           
+    Returns:     
+        Individual: The selected individual       
+    """
+    # Sorts the population in ascending order by fitness      
     sorted_population = sorted(population.individuals, key=attrgetter("fitness"), reverse=True)
     
     # Calculate cumulative probabilities for each individual based on their ranks.
     total_ranks = sum([i for i in range(1, len(sorted_population) + 1)])
     cumulative_probabilities = [(len(sorted_population) - i) / total_ranks for i in range(0, len(sorted_population))]
     
-    # Generate a random number
+    # Generates a random number
     random_num = random.uniform(0, 1)
     
-    # Perform rank selection
+    # Performs rank selection
     cumulative_sum = 0
     for i, chromo in enumerate(sorted_population):
         cumulative_sum += cumulative_probabilities[i]
-        if cumulative_sum >= random_num:
-            return chromo
+        if cumulative_sum >= random_num: 
+                if population.optim == "max":                   
+                    return chromo
+                return min(sorted_population, key=attrgetter("fitness"))
